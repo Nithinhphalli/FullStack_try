74ac32008f785874d5ad48a8e7d46f1c87a61f36
Nithinhphalli <nithinnithu415@gmail.com>
2023-07-26 00:30:49 +0530

    Learning

---
+++ First_web/JS/one.js
@@ -0,0 +1,30 @@
+let a = function()
+{
+    console.log("HI");
+}
+a();
+function abs()
+{
+    console.log("GM");
+}
+abs();
+var cz= ()=>{
+    console.log("GA");
+}
+cz();
+let add = ()=>{
+console.log("Addition");
+
+}
+add();
+let sub = (a,b)=>{
+    return a+b;
+}
+let k =sub(10,12);
+console.log(k);
+
+let message= a=>"HI " +a;
+console.log(message("GM"));
+
+let addition = a=> a;
+console.log(addition(10));
---
+++ First_web/JS/two.js
@@ -0,0 +1,4 @@
+let name="Rjkjiji"
+let salary=10234;
+console.log(`Nmae is ${name} salary is ${salary}`);
+console.log("salary is "+salary+"name is "+name);
